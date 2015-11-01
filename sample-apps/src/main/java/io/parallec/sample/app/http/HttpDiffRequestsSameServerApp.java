caea281f66fe4e56e4e3924f19ce6df75d34e053
Jeff Pei <ypei@ebay.com>
2015-11-01 13:25:13 -0800

    update

--- sample-apps/src/main/java/io/parallec/sample/app/http/HttpDiffRequestsSameServerApp.java
+++ sample-apps/src/main/java/io/parallec/sample/app/http/HttpDiffRequestsSameServerApp.java
@@ -10,7 +10,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package io.parallec.sample.app.http.request.template;
+package io.parallec.sample.app.http;
 
 import io.parallec.core.FilterRegex;
 import io.parallec.core.ParallecResponseHandler;
--- sample-apps/src/main/java/io/parallec/sample/app/http/request/template/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Sample Apps with different requersts to same/different servers, leveraging the request template and variable replacement
- * @author ypei
- *
- */
-package io.parallec.sample.app.http.request.template;
